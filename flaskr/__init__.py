364bdc03dfe917235a28be3ebab6c9efa15afe50
unknown <esteban.olaya@dominioestudio.com>
2023-10-24 12:17:34 -0500

    FIX: __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_cors import CORS
 
 
-def create_app(__name__):
+def create_app():
     # create and configure the app
     app = Flask(__name__)
     app.config.from_mapping(
