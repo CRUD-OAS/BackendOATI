26dc718497db9c72ee7e3580726cda0d5b44b6a9
unknown <esteban.olaya@dominioestudio.com>
2023-10-24 12:35:36 -0500

    FIX: __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 from flask_cors import CORS
 
 
-def create_app():
+def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
