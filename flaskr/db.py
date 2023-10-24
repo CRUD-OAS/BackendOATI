ba2845c1f20df8f9a410794412eb819449da3daf
unknown <esteban.olaya@dominioestudio.com>
2023-10-24 13:48:13 -0500

    FIX: __init__.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -25,7 +25,7 @@ def get_db():
 
 def init_db():
     db = get_db()
-    with current_app.open_resource(os.path.abspath('schema.sql')) as f:
+    with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
        
 
